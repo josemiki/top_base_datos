ae5fb638a328fc181e4a4165a9035fc44f3e54cd
josemiki <tesistapre2.unsa202016@gmail.com>
2018-04-23 11:52:52 -0500

    Modificacion del dia  viernes

--- distancias_1.cpp
+++ distancias_1.cpp
@@ -1,18 +1,26 @@
 #include<iostream>
 #include<string>
+#include<fstream>
+#include<cstdio>
 #include<unordered_map>
 #include<cmath>
+#include<vector>
+#include<algorithm>
+
 using namespace std;
+
+#define myvec vector < MyStruct >
+#define size_m 8
 
 
 float matrix[8][8]={{3.5,2.0,5.0,3.0,0.0,0.0,5.0,3.0},
-                        {2.0,3.5,1.0,4.0,4.0,4.5,2.0,0.0},
-                        {0.0,4.0,1.0,4.5,1.0,4.0,0.0,0.0},
-                        {4.5,0.0,3.0,0.0,4.0,5.0,3.0,5.0},
-                        {5.0,2.0,5.0,3.0,0.0,5.0,5.0,4.0},
-                        {1.5,3.5,1.0,4.5,0.0,4.5,4.0,2.5},
-                        {2.5,0.0,0.0,4.0,4.0,4.0,5.0,3.0},
-                        {2.0,3.0,0.0,2.0,1.0,4.0,0.0,0.0}};
+                    {2.0,3.5,1.0,4.0,4.0,4.5,2.0,0.0},
+                    {0.0,4.0,1.0,4.5,1.0,4.0,0.0,0.0},
+                    {4.5,0.0,3.0,0.0,4.0,5.0,3.0,5.0},
+                    {5.0,2.0,5.0,3.0,0.0,5.0,5.0,4.0},
+                    {1.5,3.5,1.0,4.5,0.0,4.5,4.0,2.5},
+                    {2.5,0.0,0.0,4.0,4.0,4.0,5.0,3.0},
+                    {2.0,3.0,0.0,2.0,1.0,4.0,0.0,0.0}};
 
 float distancia_manhattan(int col1,int col2)
 {
@@ -65,13 +73,22 @@ float correlacion_pearson(int col1, int col2)
     float sum_y=0.0;
     float sum_c_x=0.0;
     float sum_c_y=0.0;
+    int n=0;
     for(int i=0;i<size_m;i++)
     {
-       sum_x=sum_x+=matrix[i][col1];
-       sum_y=sum_y+=matrix[i][col2]; 
-       sum_x_m_y=sum_x_m_y+=(matrix[i][col1]*matrix[i][col2]);
-       sum_c_x=sum_c_x+=pow(matrix[i][col1],2.0);
-       sum_c_y=sum_c_y+=pow(matrix[i][col2],2.0);
+
+           
+        if(matrix[i][col1]!=0.0&&matrix[i][col2]!=0.0)
+        {
+            
+           sum_x=sum_x+=matrix[i][col1];
+           sum_y=sum_y+=matrix[i][col2]; 
+           sum_c_x=sum_c_x+=pow(matrix[i][col1],2.0);
+           sum_c_y=sum_c_y+=pow(matrix[i][col2],2.0);
+           sum_x_m_y=sum_x_m_y+=(matrix[i][col1]*matrix[i][col2]);
+           n++;
+           
+        }
     }
     
     //cout<<"sum_x:\t"<<sum_x<<endl;
@@ -80,17 +97,89 @@ float correlacion_pearson(int col1, int col2)
     //cout<<"sum_c_x:\t"<<sum_c_x<<endl;
     //cout<<"sum_c_y:\t"<<sum_c_y<<endl;
 
-    float resultado=(sum_x_m_y-((sum_x*sum_y)/size_m))/((sqrt(sum_c_x-(pow(sum_x,2.0)/size_m)))*(sqrt(sum_c_y-(pow(sum_y,2.0)/size_m))));
+    float resultado=(sum_x_m_y-((sum_x*sum_y)/n))/
+    ((sqrt(sum_c_x-(pow(sum_x,2.0)/n)))*(sqrt(sum_c_y-(pow(sum_y,2.0)/n))));
     
+    return resultado;
+}
+struct MyStruct
+{
+    int key;
+    float euclidia;
+    float pearson;
+    float influencia;
+
+    MyStruct(int k, float e,float p,float i) : key(k), euclidia(e), pearson(p),influencia(i) {}
+};
+
+struct less_than_key
+{
+    inline bool operator() (const MyStruct& struct1, const MyStruct& struct2)
+    {
+        return (struct1.euclidia > struct2.euclidia);
+    }
+};
+myvec k_nn(int col,int k)
+{
+    myvec vec;
+    for(int i =0;i<size_m;i++)
+    {
+        vec.push_back(MyStruct(i,correlacion_pearson(col,i),0.0,0.0)); 
+    }
+    std::sort(vec.begin(), vec.end(), less_than_key());
+    
+    //for(int i=0;i<25;i++)
+    //    cout<<vec[i].key<<","<<vec[i].euclidia<<"."<<vec[i].pearson<<endl;
+    int fin=(vec.size()-k);
+    for(int i=0;i<fin;i++)
+        vec.pop_back();
+    //cout<<"vec_size"<<vec.size()<<endl;
+    return vec;
+
+}
+void calcular_influencia(int col, vector < MyStruct >& score)
+{
+    float suma_pearson=0.0;
+    for(int i=0;i<score.size();i++)
+    {
+        score[i].pearson=correlacion_pearson(col,score[i].key);
+        // cout<<suma_pearson<<endl;
+        suma_pearson+=score[i].pearson;
+    }
+    //cout<<suma_pearson<<endl;
+    for(int i=0;i<score.size();i++)
+    {
+        //score[i].influencia=score[i].pearson/suma_pearson*100;
+        score[i].influencia=score[i].pearson/suma_pearson;
+    }
+
+}
+
+float proyectado_knn(myvec & knn,int libro)
+{
+    int k= knn.size();
+    float resultado=0.0;
+    //float * puntaje=new float[k];
+    for(int i=0;i<k;i++)
+    {
+        resultado+=resultado+knn[i].influencia*matrix[libro][(knn[i].key)];
+    }
     return resultado;
 }
 int main()
 {
-    
-    cout<<"Distancia Manhattan:\t"<<distancia_manhattan(0,3)<<endl;
-    cout<<"Distancia Euclidea:\t"<<distancia_euclidea(0,3)<<endl;
-    cout<<"Distancia minkowski:\t"<<distancia_minkowski(0,3,1.0)<<endl;
-    cout<<"Similitud Coseno:\t"<<similitud_coseno(4,7)<<endl;
-    cout<<"Correlacion Pearson:\t"<<correlacion_pearson(0,1)<<endl;
+    cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(0,1)<<endl;
+
+    cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(0,4)<<endl;
+    cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(0,5)<<endl;
+    /*myvec knn=k_nn(5,2);
+    calcular_influencia(5,knn);
+    for(int i=0;i<knn.size();i++)
+    {
+            cout<<knn[i].key<<" ; "<<knn[i].euclidia<<" ; "<<knn[i].pearson<<" ; "<<knn[i].influencia;
+
+        cout<<endl;
+    }
+    cout<<"proyeccion:\t"<<proyectado_knn(knn,1)<<endl;*/
     return 0;
 }
--- distancias_2.cpp
+++ distancias_2.cpp
@@ -62,7 +62,6 @@ float distancia_euclidea(int col1,int col2)
 	    resultado=resultado+=(pow((matrix[i][col1]-matrix[i][col2]),2.0));
     }
     return sqrt(resultado);
-//    return pow(resultado,0.5);
 }
 float distancia_minkowski(int col1,int col2,float t)
 {
@@ -168,13 +167,22 @@ float correlacion_pearson(int col1, int col2)
     float sum_y=0.0;
     float sum_c_x=0.0;
     float sum_c_y=0.0;
+    int n=0;
     for(int i=0;i<size_m;i++)
     {
-       sum_x=sum_x+=matrix[i][col1];
-       sum_y=sum_y+=matrix[i][col2]; 
-       sum_x_m_y=sum_x_m_y+=(matrix[i][col1]*matrix[i][col2]);
-       sum_c_x=sum_c_x+=pow(matrix[i][col1],2.0);
-       sum_c_y=sum_c_y+=pow(matrix[i][col2],2.0);
+
+           
+        if(matrix[i][col1]!=0.0&&matrix[i][col2]!=0.0)
+        {
+            
+           sum_x=sum_x+=matrix[i][col1];
+           sum_y=sum_y+=matrix[i][col2]; 
+           sum_c_x=sum_c_x+=pow(matrix[i][col1],2.0);
+           sum_c_y=sum_c_y+=pow(matrix[i][col2],2.0);
+           sum_x_m_y=sum_x_m_y+=(matrix[i][col1]*matrix[i][col2]);
+           n++;
+           
+        }
     }
     
     //cout<<"sum_x:\t"<<sum_x<<endl;
@@ -183,7 +191,8 @@ float correlacion_pearson(int col1, int col2)
     //cout<<"sum_c_x:\t"<<sum_c_x<<endl;
     //cout<<"sum_c_y:\t"<<sum_c_y<<endl;
 
-    float resultado=(sum_x_m_y-((sum_x*sum_y)/size_m))/((sqrt(sum_c_x-(pow(sum_x,2.0)/size_m)))*(sqrt(sum_c_y-(pow(sum_y,2.0)/size_m))));
+    float resultado=(sum_x_m_y-((sum_x*sum_y)/n))/
+    ((sqrt(sum_c_x-(pow(sum_x,2.0)/n)))*(sqrt(sum_c_y-(pow(sum_y,2.0)/n))));
     
     return resultado;
 }
@@ -201,7 +210,7 @@ struct less_than_key
 {
     inline bool operator() (const MyStruct& struct1, const MyStruct& struct2)
     {
-        return (struct1.euclidia < struct2.euclidia);
+        return (struct1.euclidia > struct2.euclidia);
     }
 };
 
@@ -210,21 +219,24 @@ myvec k_nn(int col,int k)
     myvec vec;
     for(int i =0;i<size_m;i++)
     {
-        vec.push_back(MyStruct(i,distancia_euclidea(col,i),0.0,0.0)); 
+        vec.push_back(MyStruct(i,correlacion_pearson(col,i),0.0,0.0)); 
     }
     std::sort(vec.begin(), vec.end(), less_than_key());
-    
     vec.erase(vec.begin());
+    //for(int i=0;i<25;i++)
+      //  cout<<vec[i].key<<","<<vec[i].euclidia<<"."<<vec[i].pearson<<endl;
+    //cout<<"ordenado"<<endl;
+    //cout<<"vec_size"<<vec.size()<<endl;
+    /*int fin=(vec.size()-k);
 
-    for(int i=0;i<25;i++)
-        cout<<vec[i].key<<","<<vec[i].euclidia<<"."<<vec[i].pearson<<endl;
-    
-    
-
+    for(int i=0;i<fin;i++)
+        vec.pop_back();
+    //cout<<"vec_size"<<vec.size()<<endl;*/
+    cout<<"fin:\t"<<vec.size()-k<<endl;
     int fin=(vec.size()-k);
     for(int i=0;i<fin;i++)
         vec.pop_back();
-    //cout<<"vec_size"<<vec.size()<<endl;
+    
     return vec;
 
 }
@@ -253,8 +265,12 @@ float proyectado_knn(myvec & knn,int libro)
     //float * puntaje=new float[k];
     for(int i=0;i<k;i++)
     {
-        resultado+=resultado+knn[i].influencia*matrix[libro][(knn[i].key)];
+
+        resultado+=knn[i].influencia*matrix[libro][(knn[i].key)];
+        cout<<knn[i].influencia*matrix[libro][(knn[i].key)]<<endl;
+
     }
+    cout<<resultado;
     return resultado;
 }
 int main()
@@ -264,29 +280,26 @@ int main()
     //cout<<"Distancia Manhattan:\t"<<distancia_manhattan(0,1)<<endl;
     //cout<<"Distancia Euclidea:\t"<<distancia_euclidea(0,1)<<endl;
     //cout<<"Distancia Euclidea:\t"<<distancia_euclidea(4,7)<<endl;
-    cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(nombres.find("Patrick C")->second,21)<<endl;
-    cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(nombres.find("Patrick C")->second,14)<<endl;
-    cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(nombres.find("Patrick C")->second,3)<<endl;
-    cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(nombres.find("Patrick C")->second,20)<<endl;
+    //cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(6,8)<<endl;
+    //cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(nombres.find("Patrick C")->second,14)<<endl;
+    //cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(nombres.find("Patrick C")->second,3)<<endl;
+    //cout<<"Correlacion de Pearson:\t"<<correlacion_pearson(nombres.find("Patrick C")->second,20)<<endl;
     //cout<<"Distancia Euclidea:\t"<<distancia_euclidea(4,5)<<endl;
     //cout<<"Distancia Euclidea:\t"<<distancia_euclidea(0,1)<<endl;
     //cout<<"Distancia minkowski:\t"<<distancia_minkowski(0,1,1.0)<<endl;
     //cout<<"Similitud Coseno:\t"<<similitud_coseno(0,7)<<endl;
-   
-    //myvec knn=k_nn(nombres.find("brian")->second,3);
-
-    myvec knn=k_nn(8,3);
-    //calcular_influencia(nombres.find("brian")->second,knn);
-    calcular_influencia(8,knn);
-    
+    //myvec knn=k_nn(nombres.find("Patrick C")->second,3);
+    myvec knn=k_nn(4,2);
+    //calcular_influencia(nombres.find("Patrick C")->second,knn);
+    calcular_influencia(4,knn);
     for(int i=0;i<knn.size();i++)
     {
             cout<<knn[i].key<<" ; "<<knn[i].euclidia<<" ; "<<knn[i].pearson<<" ; "<<knn[i].influencia;
 
         cout<<endl;
     }
-    //cout<<"proyeccion:\t"<<proyectado_knn(knn,libros.find("Forest Gump")->second)<<endl;
-    cout<<"proyeccion:\t"<<proyectado_knn(knn,6)<<endl;
+    //cout<<"proyeccion:\t"<<proyectado_knn(knn,libros.find("Avatar")->second)<<endl;
+    cout<<"proyeccion:\t"<<proyectado_knn(knn,9)<<endl;
 
     return 0;
 }
