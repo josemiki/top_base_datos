39c8ab0fc8b54545b277840f64bfa7959cdfb1c9
josemiki <tesistapre2.unsa202016@gmail.com>
2018-04-26 14:55:10 -0500

    load data 2

--- tarea_3/tarea3.cpp
+++ tarea_3/tarea3.cpp
@@ -62,6 +62,99 @@ void load_hash_header()
     
 
 }
+void load_matrix_file()
+{
+ ifstream input("../../bd/BX-Book-Ratings.csv");
+    string u,b,r;
+    
+    float rat;
+    int line=1;
+    while(!input.eof())
+    {
+        getline(input,u,';');
+        if(!u.empty())
+        {
+            //cout<<line<<endl;
+            getline(input,b,';');
+            getline(input,r);
+            //cout<<"line: "<<line<<" user: "<<u<<" book: "<<b<<" rat: "<<r.size()<<endl;
+            //r.erase(0,1);
+            //erase(r.size()-1,1);
+            //cout<<"line: "<<line<<" user: "<<u<<" book: "<<b<<" rat: "<<r<<endl;
+            rat=stof(r);
+            
+            //fila_b fb;
+            //fb.insert({b,rat});
+
+            //fila_u fu;
+            //fu.insert({u,rat});
+            
+       
+
+            
+                auto it_f_b=matrix_user.find(u);
+                auto it_f_u=matrix_book.find(b);
+                if(it_f_b!=matrix_user.end())
+                {
+                    //cout<<"entro if"<<endl;
+                    //cout<<"it_f_b: "<<it_f_b->first<<" , "<<it_f_b->second->size()<<endl;
+                    it_f_b->second->insert({b,rat});
+                    //cout<<"salio if"<<endl;
+                }
+                else
+                {
+                    //cout<<"entro else"<<endl;
+                    fila_b* fb=new fila_b();
+                    fb->insert({b,rat});
+                    matrix_user.insert({u,fb});
+                    //cout<<"salio else"<<endl;
+                }
+                if(it_f_u!=matrix_book.end())
+                {
+                    //cout<<"entro if"<<endl;
+                    //cout<<"it_f_b: "<<it_f_b->first<<" , "<<it_f_b->second->size()<<endl;
+                    it_f_u->second->insert({u,rat});
+                    //cout<<"salio if"<<endl;
+                }
+                else
+                {
+                    //cout<<"entro else"<<endl;
+                    fila_u* fu=new fila_u();
+                    fu->insert({u,rat});
+                    matrix_book.insert({b,fu});
+                    //cout<<"salio else"<<endl;
+                }
+                //matrix_book.insert({b,fu});
+            
+            //cout<<"line: "<<line<<" user: "<<u<<" book: "<<b<<" rat: "<<rat<<endl;
+            ++line;
+        }
+
+    }
+
+    auto it=matrix_user.begin();
+    int size_hash_y=0;
+    while(it!=matrix_user.end())
+    {
+        //cout<<it->first<<" size hash: "<<(it->second)->size()<<endl;
+
+        auto it2=it->second->begin();
+        while(it2!=it->second->end())
+        {
+            //cout<<"   "<<it2->first<<" , "<<it2->second<<"    ";
+            size_hash_y++;
+            it2++;
+        }
+        //cout<<endl;
+        it++;
+    }
+    cout<<"size_hash:\t"<<size_hash_y<<endl;
+
+    cout<<"load matriz users \n size_matrix_user: "<<matrix_user.size()<<endl;
+    cout<<"load matriz books \n size_matrix_book: "<<matrix_book.size()<<endl;
+
+}
+
 void load_matrix()
 {
     ifstream input("../../bd/BX-Book-Ratings.csv");
@@ -212,7 +305,8 @@ float correlacion_pearson(string col1, string col2)
     int n=0;
     
     auto itc1=matrix_user.find(col1); 
-    auto itc2=matrix_user.find(col2);    
+    auto itc2=matrix_user.find(col2);
+
     auto it_matrix_end=matrix_user.end();
     auto itf1=itc1->second->begin();
 
@@ -225,25 +319,38 @@ float correlacion_pearson(string col1, string col2)
             //cout<<"entro 1 if"<<endl;
             if(itf1->first==itf2->first)
             {
-                sum_x=sum_x+=itf1->second;
-                sum_y=sum_y+=itf2->second; 
-                sum_c_x=sum_c_x+=pow(itf1->second,2.0);
-                sum_c_y=sum_c_y+=pow(itf2->second,2.0);
-                sum_x_m_y=sum_x_m_y+=(itf1->second*itf2->second);
+                //cout<<itf1->second<<","<<itf2->second<<endl;
+
+                sum_x+=itf1->second;
+                //cout<<"sum_x: "<<sum_x<<endl;
+                
+                sum_y+=itf2->second; 
+                //cout<<"sum_y: "<<sum_y<<endl;
+
+                sum_c_x+=pow(itf1->second,2.0);
+                //cout<<"sum_c_x: "<<sum_c_x<<endl;
+                
+                sum_c_y+=pow(itf2->second,2.0);
+                //cout<<"sum_c_y: "<<sum_c_y<<endl;
+                
+                sum_x_m_y+=(itf1->second*itf2->second);
+                //cout<<"sum_x_m_y: "<<sum_x_m_y<<endl;
+
                 n++;
-                //cout<<itf2->second<<endl;
-                //resultado+=pow((itf1->second - itf2->second),2.0);
+                //cout<<"n: "<<n<<endl;
+                
             }
+           
             
         }
+         //cout<<"===========>"<<endl;
         //cout<<"no hay coincidencia"<<endl;
         itf1++; 
     }
     
-
-    resultado=(sum_x_m_y-((sum_x*sum_y)/n))/
-    ((sqrt(sum_c_x-(pow(sum_x,2.0)/n)))*(sqrt(sum_c_y-(pow(sum_y,2.0)/n))));
     
+    resultado=(sum_x_m_y-((sum_x*sum_y)/n))/((sqrt(sum_c_x-(pow(sum_x,2.0)/n)))*(sqrt(sum_c_y-(pow(sum_y,2.0)/n))));
+    //cout<<"resultado de Pearson:\t"<<resultado<<endl;
     return resultado;
 }
 struct MyStruct
@@ -333,167 +440,25 @@ float proyectado_knn(myvec & knn,string libro)
 int main()
 {
     load_hash_header();
-    load_matrix();
-    cout<<"distancia_euclidea: "<<distancia_euclidea("15600","15651");
-    cout<<"distancia_euclidea: "<<correlacion_pearson("15600","15651");
-    myvec knn=k_nn("15600",25000);
-    calcular_influencia("15600",knn);
+    load_matrix_file();
+    //cout<<"distancia_euclidea: "<<distancia_euclidea("15600","15651");
+    //cout<<"distancia_euclidea: "<<correlacion_pearson("15600","15651");
+    myvec knn=k_nn("121452",5);
     for(int i=0;i<knn.size();i++)
     {
             cout<<knn[i].key<<" ; "<<knn[i].euclidia<<" ; "<<knn[i].pearson<<" ; "<<knn[i].influencia;
 
         cout<<endl;
     }
+    /*calcular_influencia("15600",knn);
+    for(int i=0;i<knn.size();i++)
+    {
+            cout<<knn[i].key<<" ; "<<knn[i].euclidia<<" ; "<<knn[i].pearson<<" ; "<<knn[i].influencia;
+
+        cout<<endl;
+    }*/
     //cout<<"proyeccion:\t"<<proyectado_knn(knn,"Lord of the Rings")<<endl;
     
     return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//Reading without comillas 
-#define fila unordered_map<string,float>
-#define columna unordered_map<int,fila>
-
-#define columnas unordered_map<int,float>
-#define filas unordered_map<string,columnas>
-
-#define users unordered_map<int,string>
-#define books unordered_map<string,string>
-
-columna matrix;
-filas matrix_fil;
-users hash_users;
-books hash_books;
-
-void load_headers()
-{
-    ifstream book("BX-Books.csv");
-    string b1,b2;
-    int line=1;
-    while(!book.eof())
-    {
-        getline(book,b1,';');
-        if(!b1.empty())
-        {
-            getline(book,b2);
-            hash_books.insert({b1,b2});
-            //cout<<"line: "<<line<<" col: "<<col<<" fil: "<<fil<<" rat: "<<rat<<endl;
-            ++line;
-        }
-    }
-    cout<<"size_books: "<<hash_books.size()<<endl;
-
-    ifstream user("BX-Users.csv");
-    line=1;
-    int i=0;
-    while(!user.eof())
-    {
-        getline(user,b1,';');
-        if(!b1.empty())
-        {
-            //cout<<b1<<endl;
-            i=stoi(b1);
-            
-            getline(user,b2);
-            hash_users.insert({i,b2});
-            
-            //cout<<"inserto:"<<line<<endl;
-            //cout<<"line: "<<line<<" col: "<<col<<" fil: "<<fil<<" rat: "<<rat<<endl;
-            ++line;
-            b2.clear();
-            b1.clear();
-
-        }
-    }
-    cout<<"size_users: "<<hash_users.size()<<endl;
-    
-
-}
-
-void load_matrix_col()
-{
-    ifstream input("BX-Book-Ratings.csv");
-    string word,fil;
-    int col;
-    float rat;
-    int line=1;
-    auto itcol=hash_users.begin();
-    auto itfil=hash_books.begin();
-    while(!input.eof())
-    {
-        getline(input,word,';');
-        if(!word.empty())
-        {
-            col=stoi(word);
-            word.clear();
-            getline(input,fil,';');
-            getline(input,word);
-            rat=stof(word);
-            fila f;
-            f.insert({fil,rat});
-            itcol=hash_users.find(col);
-            itfil=hash_books.find(fil);
-            if(itcol!=hash_users.end()&&itfil!=hash_books.end())
-                matrix.insert({col,f});
-            //cout<<"line: "<<line<<" col: "<<col<<" fil: "<<fil<<" rat: "<<rat<<endl;
-            ++line;
-        }
-    }
-    cout<<"size_matrix: "<<matrix.size()<<endl;
-
-}
-
-void load_matrix_fil()
-{
-    ifstream input("BX-Book-Ratings.csv");
-    string word,fil;
-    int col;
-    float rat;
-    int line=1;
-    auto itcol=hash_users.begin();
-    auto itfil=hash_books.begin();
-    while(!input.eof())
-    {
-        getline(input,word,';');
-        if(!word.empty())
-        {
-            col=stoi(word);
-            word.clear();
-            getline(input,fil,';');
-            getline(input,word);
-            rat=stof(word);
-            columnas c;
-            c.insert({col,rat});
-            itcol=hash_users.find(col);
-            itfil=hash_books.find(fil);
-            if(itcol!=hash_users.end()&&itfil!=hash_books.end())
-                matrix_fil.insert({fil,c});
-            //cout<<"line: "<<line<<" col: "<<col<<" fil: "<<fil<<" rat: "<<rat<<endl;
-            ++line;
-        }
-    }
-    cout<<"size_matrix_fil: "<<matrix_fil.size()<<endl;
-
-}
-*/
